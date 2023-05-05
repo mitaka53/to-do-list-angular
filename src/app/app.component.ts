64967d7bebee18b5d6de2fb47206355f2557479c
mitaka53 <34245881+mitaka53@users.noreply.github.com>
2023-05-05 11:47:26 +0300

    Add list of sample tasks

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,25 @@
 import { Component } from '@angular/core';
+import { Task } from './Task';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
 })
 export class AppComponent {
   title = 'to-do-list-angular';
+
+  allTasks: Task[] = [
+    {
+      title: 'Walk the dog',
+      description: 'Walking the dog around the block',
+      completed: false,
+      dateAdded: new Date(),
+      dateUpdated: new Date(),
+    },
+  ];
+
+  get tasks() {
+    return this.allTasks;
+  }
 }
